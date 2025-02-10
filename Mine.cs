d05b159a71ca4c34f5894bdbab4641551d0cf84b
oliverslott <oliverslottsony@gmail.com>
2025-02-10 11:49:15 +0100

    Create Mine.cs

---
+++ Mine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace warcraft_4
+{
+    internal class Mine : GameObject
+    {
+        private Semaphore semaphore = new Semaphore(3, 3);
+        
+        public void Enter()
+        {
+            Debug.WriteLine("Worker is trying to enter the mine");
+            semaphore.WaitOne();
+            Debug.WriteLine("Worker has entered the mine");
+        }
+
+        public void Exit()
+        {
+            semaphore.Release();
+            Debug.WriteLine("Worker has exited the mine");
+        }
+    }
+}
