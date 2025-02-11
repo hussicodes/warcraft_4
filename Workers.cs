8ba7e9439e11fabae547c6b1cd538767cfa7fa8b
hussicodes <137882001+hussicodes@users.noreply.github.com>
2025-02-11 12:25:39 +0100

    Threads has been added to workers

--- Workers.cs
+++ Workers.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace warcraft_4
@@ -14,33 +15,67 @@ namespace warcraft_4
         private Texture2D[] idleTexture;
         private int counter;
         private int activeFrame;
+
+        Thread myThreads;
+
+        public Workers()
+        {
+            myThreads = new Thread(Update2);
+            myThreads.IsBackground = true;
+            
+
+        }
+
         public override void LoadContent(ContentManager contentManager)
         {
             idleTexture = new Texture2D[3];
             idleTexture[0] = contentManager.Load<Texture2D>("Pawn_Blue1");
             idleTexture[1] = contentManager.Load<Texture2D>("Pawn_Blue2");
             idleTexture[2] = contentManager.Load<Texture2D>("Pawn_Blue3");
+
+            myThreads.Start();
+
         }
 
         public override void Update(GameTime gameTime)
         {
-            counter++;
+            //counter++;
 
-            if (counter > 29)
-            {
-                counter = 0;
-                activeFrame++;
+            //if (counter > 29)
+            //{
+            //    counter = 0;
+            //    activeFrame++;
 
-                if (activeFrame > idleTexture.Length - 1)
-                {
-                    activeFrame = 0;
-                }
-            }
+            //    if (activeFrame > idleTexture.Length - 1)
+            //    {
+            //        activeFrame = 0;
+            //    }
+            //}
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(idleTexture[activeFrame], new Rectangle(60, 60, 60, 60), Color.White);
+            spriteBatch.Draw(idleTexture[activeFrame], new Rectangle(56, 56, 56, 56), Color.White);
+        }
+
+        public void Update2(object data)
+        {
+            while (true)
+            {
+                Thread.Sleep(16);
+                counter++;
+                if (counter > 29)
+                {
+                    counter = 0;
+                    activeFrame++;
+
+                    if (activeFrame > idleTexture.Length - 1)
+                    {
+                        activeFrame = 0;
+                    }
+                    
+                }
+            }
         }
     }
 }
