f8adcba4a981beda5adfbd210445a5ccdaac20a8
Muddi02 <144444928+Muddi02@users.noreply.github.com>
2025-02-12 15:08:37 +0100

    Add files via upload

---
+++ Lesgo/Game1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct2D1;
+using SharpDX.XInput;
+
+namespace Lesgo
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch;
+        private Tileset tileset;
+        private Map map;
+        
+        // below are remnants from an alpha build
+        Texture2D texture;
+        Texture2D Tileset;
+        Rectangle rectangle;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+           
+
+            
+            int tileSize = 32; // Here you can change the size of your tiles, make em big or make em small, remember to adjust in map class aswell!
+
+            int screenWidth = graphics.PreferredBackBufferWidth;
+            int screenHeight = graphics.PreferredBackBufferHeight;
+
+            graphics.PreferredBackBufferWidth = 1300;  // Sets width of program screen, NOT game screen! Adjust in Map too or you get Nintendo DS effect
+            graphics.PreferredBackBufferHeight = 600; // Sets height of program screen, NOT game screen! Adjust in Map too or you get Nintendo DS effect
+            graphics.ApplyChanges();
+            // Calculate how many tiles fit on the screen
+            int tilesX = screenWidth / tileSize;
+            int tilesY = screenHeight / tileSize;
+
+            
+            tileset = new Tileset(Content.Load<Texture2D>("tileset"));
+
+            // Create the map with the calculated size
+            map = new Map(tilesX, tileset);
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new Microsoft.Xna.Framework.Graphics.SpriteBatch(GraphicsDevice);
+
+
+            Texture2D tilesetTexture = Content.Load<Texture2D>("tileset");
+            tileset = new Tileset(tilesetTexture);
+            tileset.AddTile(224, 16, 16, 16);
+            tileset.AddTile(240, 16, 16, 16);
+            tileset.AddTile(224, 16, 16, 16);
+            tileset.AddTile(240, 16, 16, 16);
+            map = new Map(32, tileset);
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+       
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DarkBlue);
+
+            spriteBatch.Begin();
+
+            
+            map.Draw(spriteBatch);
+
+
+            spriteBatch.End();
+
+       
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Lesgo/Map.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace Lesgo
+{
+    struct Tile
+    {
+        public Rectangle DrawRectangle; // This is the selected tile image
+    }
+
+    public class Map
+    {
+        public int Size;
+        public int TileSize;
+        public Tileset Tileset;
+
+        private Rectangle[][] tileGrid;
+        private Random r = new Random();
+
+        // Constructor
+        public Map(int size, Tileset tileset)
+        {
+            Size = size;
+            TileSize = 32; // Size of tile! If you want to edit, also edit in game1
+            Tileset = tileset;
+
+            // Figures out how many tiles that fit horizontally and vertically
+            int screenWidth = 1300;  // sets scren width, remember to adjust in game1 too, or you will get a Nintendo DS experience
+            int screenHeight = 600; // sets screen height, remember to adjust in game1 too, or you will get a Nintendo DS experience
+
+            // The map will fill the screen, so calculate the number of tiles
+            int tilesX = screenWidth / TileSize;
+            int tilesY = screenHeight / TileSize;
+
+            Size = Math.Max(tilesX, tilesY); // Adjust size
+
+            // Initialize the grid with random tiles, but always the same tiles
+            tileGrid = new Rectangle[Size][];
+            for (int i = 0; i < Size; i++)
+            {
+                tileGrid[i] = new Rectangle[Size];
+                for (int j = 0; j < Size; j++)
+                {
+                    // Pick a random tile from the Tileset
+                    var tile = Tileset.TileRegions[r.Next(Tileset.TileRegions.Count)];
+                    tileGrid[i][j] = tile;
+                }
+            }
+        }
+
+        // Draw method to draw the map to the screen
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Loop through the grid and draw each tile
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    // Used to find where the next Rectangle will be drawn
+                    Rectangle destinationRectangle = new Rectangle(i * TileSize, j * TileSize, TileSize, TileSize);
+
+                    // Draw the tile using our tileset
+                    spriteBatch.Draw(Tileset.TilesetTexture, destinationRectangle, tileGrid[i][j], Color.White);
+                }
+            }
+        }
+    }
+}
